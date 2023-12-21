3dcc7f571b4019e1ebfc27273f3046ba2bea4f9f
ulinnia <ryan98244@gmail.com>
2023-12-21 13:13:47 +0000

    Explain

--- ChangeTheOutput.c
+++ ChangeTheOutput.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 #include<math.h>
 
-void calculate(int *a, int *b)
+void calculate(int *a, int *b)  //接收a和b的位址(int *)
 {
-    int temp = *a;
-    *a = pow(*a,*b);
-    *b = pow(*b,temp);
+    int temp = *a;              //temp暫存a的值(*)
+    *a = pow(*a,*b);            //將兩個位址的值做次方運算
+    *b = pow(*b,temp);          //位址的值改變，main函數裡a、b的值跟著改變
 }
 
 int main(){
     int a,b;
-    int *ptr = &a;
-    scanf("%d %d",&a,&b);
-    calculate(&a,&b);
-    printf("%d %d\n",a,b);
+    scanf("%d %d",&a,&b);   //輸入兩個數
+    calculate(&a,&b);       //將a的位址b的位址丟進calculate函數運算
+    printf("%d %d\n",a,b);  //印出a的b次方，和b的a次方
 }
